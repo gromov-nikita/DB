5849751652571e358ebcce09d8f7ce7108cf3ca9
gromov-nikita <hromay.n999@gmail.com>
2021-08-20 19:26:01 +0300

    1

--- dbJava/src/Runner.java
+++ dbJava/src/Runner.java
@@ -6,10 +6,12 @@ public class Runner {
                     DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "log",
                             "root","123456");
             Statement statement = connection.createStatement();
-            int result = statement.executeUpdate("INSERT INTO app SET name = 'Java6'");
-            ResultSet res = statement.executeQuery("select * from app");
+            int result = statement.executeUpdate("INSERT INTO user SET login = 'Java1', password = '123456qwe', " +
+                    "firstname = 'Ник', lastname = 'Гром', role = 'ADMIN';");
+            ResultSet res = statement.executeQuery("select * from user");
             while(res.next()) {
-                System.out.println(res.getString("AppID") + " " + res.getString("name"));
+                System.out.println(res.getString("UserID") + " "
+                        + res.getString("firstname") + " " + res.getString("lastname"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
