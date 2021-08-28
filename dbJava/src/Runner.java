3c2f99d4997d353b9c016ba5ac8cfc6684ba554a
gromov-nikita <hromay.n999@gmail.com>
2021-08-28 16:56:02 +0300

    1

--- dbJava/src/Runner.java
+++ dbJava/src/Runner.java
@@ -6,13 +6,14 @@ public class Runner {
         try {
             DBConnection connection =
                     new DBConnection("root","123456", "jdbc:mysql://localhost:3306/" + "log");
-            connection.pushQuery("INSERT INTO user SET login = 'Java1', password = '123456qwe', " +
+            connection.pushQuery("INSERT INTO user SET login = 'Java13', password = '123456qwe', " +
                     "firstname = 'Ник', lastname = 'Гром', roleID = 1;");
             ResultSet res = connection.getQuery("select * from user");
             while(res.next()) {
                 System.out.println(res.getString("UserID") + " "
                         + res.getString("firstname") + " " + res.getString("lastname"));
             }
+            connection.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- dbJava/src/db/DBConnection.java
+++ dbJava/src/db/DBConnection.java
@@ -1,38 +1,45 @@
 package db;
 
+import java.io.IOException;
 import java.sql.*;
-
+import java.util.HashMap;
+import  java.util.logging.*;
 public class DBConnection {
+    private static Logger log = Logger.getLogger(DBConnection.class.getName());
     private Connection connection;
     private Statement statement;
-    public DBConnection(String login, String password, String url) {
+    private String nameDB;
+    static {
         try {
-            connection = DriverManager.getConnection(url, login, password);
-            statement = connection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Handler handler = new FileHandler("src/db/log/log.txt",true);
+            handler.setFormatter(new SimpleFormatter());
+            log.addHandler(handler);
+            log.setUseParentHandlers(false);
+
+        } catch (IOException e) {
+            log.log(Level.WARNING,"File logger not working");
         }
     }
-    public void pushQuery(String query) {
-        try {
-            statement.executeUpdate(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public DBConnection(String login, String password, String url) throws SQLException {
+        String[] arr = url.split("/");
+        nameDB = arr[arr.length - 1];
+        connection = DriverManager.getConnection(url, login, password);
+        log.info(nameDB + " database. Created a connection." );
+        statement = connection.createStatement();
+        log.info(nameDB + " database. Created a statement.");
     }
-    public ResultSet getQuery(String query) {
-        try {
-            return statement.executeQuery(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+
+    public void pushQuery(String query) throws SQLException {
+        statement.executeUpdate(query);
+        log.info(nameDB + " database. Query: " + query);
     }
-    public void closeConnection() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public ResultSet getQuery(String query) throws SQLException {
+        ResultSet res = statement.executeQuery(query);
+        log.info(nameDB + " database. Query: " + query);
+        return res;
+    }
+    public void closeConnection() throws SQLException {
+        connection.close();
+        log.info(nameDB + "database connection. Close. ");
     }
 }
